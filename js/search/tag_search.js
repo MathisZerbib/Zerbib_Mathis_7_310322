31b98d717e922487fdc4231254f7d4afd3aabec4
Mathis Zerbib <mathis.zerbib@gmail.com>
2022-10-15 16:02:41 +0200

    filter by appliance and ingredient

--- js/search/tag_search.js
+++ js/search/tag_search.js
@@ -8,31 +8,38 @@ const tagArraySearch = (searchedTags, data) => {
     searchedTags.appliance.length +
     searchedTags.ustensil.length;
 
-  for (i = 0; finalLength !== i; i++) {
+  for (i = 0; searchedTags.ingredient.length !== i; i++) {
     filteredRecipes = tempArray.filter(
       (recipe) =>
-        !searchedTags.ingredient.length ||
-        (recipe.ingredients.find((ingredient) =>
+        !searchedTags.ingredient[i] ||
+        recipe.ingredients.find((ingredient) =>
           cleanString(ingredient.ingredient).includes(
             cleanString(searchedTags.ingredient[i])
           )
-        ) 
-        
-        
-        // &&
-        //   (!searchedTags.appliance.length ||
-        //     cleanString(recipe.appliance).includes(
-        //       cleanString(searchedTags.appliance[i])
-        //     )) &&
-        //   (!searchedTags.ustensil.length ||
-        //     recipe.ustensils.find((ustensil) => {
-        //       cleanString(ustensil).includes(
-        //         cleanString(searchedTags.ustensil[i])
-        //       );
-        //     })))
-    ));
-
+        )
+    );
     console.log("filteredRecipes", filteredRecipes, searchedTags);
+  }
+
+  for (i = 0; searchedTags.appliance.length !== i; i++) {
+    filteredRecipes = tempArray.filter(
+      (recipe) =>
+        !searchedTags.appliance[i] ||
+        cleanString(recipe.appliance).includes(
+          cleanString(searchedTags.appliance[i])
+        )
+    );
+  }
+
+  for (i = 0; searchedTags.ustensil.length !== i; i++) {
+    filteredRecipes = tempArray.filter(
+      (recipe) =>
+        !searchedTags.ustensil[i] ||
+        recipe.ustensils.find((ustensil) => {
+          cleanString(ustensil).includes(cleanString(searchedTags.ustensil[i]));
+        })
+    );
+  }
 
     if (
       (filteredRecipes &&
@@ -45,8 +52,9 @@ const tagArraySearch = (searchedTags, data) => {
       tempArray = filteredRecipes;
       console.log("temp array", tempArray);
     }
-  }
   displayData(filteredRecipes);
-
+  if (filteredRecipes.length == 0) {
+    showError();
+  }
   return filteredRecipes;
 };
