53dcab07598d57f2e839a8a989936b828b9febc4
Mathis Zerbib <mathis.zerbib@gmail.com>
2022-10-17 15:51:51 +0200

    replace th build tag array switch case :fork_and_knife:

--- js/dom/build_tag_array.js
+++ js/dom/build_tag_array.js
@@ -1,137 +1,140 @@
 const buildTagArray = (inputField, tags, id) => {
-    for (let i = 0; ulLength(tags) > i; i++) {
-        let liTag = document.createElement('li');
-        let cleanTagName = ''
-        liTag.setAttribute('aria-selected', 'false');
-        liTag.setAttribute('role', 'option');
-        liTag.innerHTML = tags[i];
-        // console.log(tags[i])
+  for (let i = 0; ulLength(tags) > i; i++) {
+    let liTag = document.createElement("li");
+    let cleanTagName = "";
+    liTag.setAttribute("aria-selected", "false");
+    liTag.setAttribute("role", "option");
+    liTag.innerHTML = tags[i];
+    // console.log(tags[i])
 
-        if (tags[i].includes('_')) {
-            cleanTagName = tags[i].replace(/\s/g, '_')
-        }else {
-            cleanTagName = tags[i]
-        }
+    if (tags[i].includes("_")) {
+      cleanTagName = tags[i].replace(/\s/g, "_");
+    } else {
+      cleanTagName = tags[i];
+    }
 
-        liTag.id = cleanTagName
+    liTag.id = cleanTagName;
 
+    let filtersboxIngredient = document.querySelector(
+      "#sub-search__Ingrédient"
+    );
 
-        let filtersboxIngredient = document.querySelector('#sub-search__Ingrédient');
+    let ulIngredient = filtersboxIngredient.querySelector(
+      ".sub-search__taglist"
+    );
 
-        let ulIngredient = filtersboxIngredient.querySelector('.sub-search__taglist')
+    let parentInputIngredient = ulIngredient.closest("div").id;
+    let strIngredient = parentInputIngredient.replace("sub-search__", "");
 
+    let filtersboxUstensil = document.querySelector("#sub-search__Ustensiles");
 
-        let parentInputIngredient = ulIngredient.closest('div').id
-        let strIngredient = parentInputIngredient.replace('sub-search__', '');
+    let ulUstensil = filtersboxUstensil.querySelector(".sub-search__taglist");
 
+    let parentInputUstensil = ulUstensil.closest("div").id;
+    let strUstensil = parentInputUstensil.replace("sub-search__", "");
 
-        let filtersboxUstensil = document.querySelector('#sub-search__Ustensiles');
+    let filtersboxAppliance = document.querySelector("#sub-search__Appareils");
 
-        let ulUstensil = filtersboxUstensil.querySelector('.sub-search__taglist')
+    let ulAppliance = filtersboxAppliance.querySelector(".sub-search__taglist");
 
+    let parentInputAppliance = ulAppliance.closest("div").id;
+    let strAppareil = parentInputAppliance.replace("sub-search__", "");
 
-        let parentInputUstensil = ulUstensil.closest('div').id
-        let strUstensil = parentInputUstensil.replace('sub-search__', '');
+    function build(type) {
+      function isIngredient() {
+        liTag.classList.add("dropdown-item", "bg-primary");
+        liTag.addEventListener("click", () => {
+          // console.log('Litag', allIngredients, cleanTagName)
+          cleanTagListDOM(strIngredient);
+          liTag.setAttribute("aria-selected", "true");
+          console.log("Tags ", liTag.innerText);
+          createTag(liTag, "bg-primary");
+          inputField.value = "";
+        });
+      }
+      function isAppliance() {
+        liTag.classList.add("dropdown-item", "bg-green");
+        liTag.addEventListener("click", () => {
+          cleanTagListDOM(strAppareil);
+          // console.log('UNIQUE APPLIANCE', uniqueAppliancesClone)
+          liTag.setAttribute("aria-selected", "true");
+          // console.log('Tags ', tags)
+          createTag(liTag, "bg-green");
+          inputField.value = "";
+        });
+      }
+      function isUstensiles() {
+        liTag.classList.add("dropdown-item", "bg-red");
+        liTag.addEventListener("click", () => {
+          cleanTagListDOM(strUstensil);
+          liTag.setAttribute("aria-selected", "true");
+          createTag(liTag, "bg-red");
+          inputField.value = "";
+        });
+      }
+      var array = {
+        Ingrédient: isIngredient,
+        Appareils: isAppliance,
+        Ustensiles: isUstensiles,
+      };
+      return array[type]();
+    }
 
+    build(id);
 
-        let filtersboxAppliance = document.querySelector('#sub-search__Appareils');
+    let finalLength =
+      tagArrayToSearch.ingredient.length +
+      tagArrayToSearch.appliance.length +
+      tagArrayToSearch.ustensil.length;
 
-        let ulAppliance = filtersboxAppliance.querySelector('.sub-search__taglist')
+    for (let y = 0; tagArrayToSearch.ingredient.length !== y; y++) {
+      if (
+        cleanString(tagArrayToSearch.ingredient[y]).includes(
+          cleanString(liTag.innerText)
+        )
+      ) {
+        console.log(
+          "Include dont build",
+          "tagArrayToSearch",
+          tagArrayToSearch.ingredient[y],
+          liTag.innerText
+        );
+      } else {
+        document.getElementById(id + "__taglist").append(liTag);
+      }
+    }
 
+    for (let y = 0; tagArrayToSearch.appliance.length !== y; y++) {
+      if (
+        cleanString(tagArrayToSearch.appliance[y]).includes(
+          cleanString(liTag.innerText)
+        )
+      ) {
+        // console.log('Include dont build', 'tagArrayToSearch',tagArrayToSearch.length, tagArrayToSearch[y], y)
+      } else {
+        document.getElementById(id + "__taglist").append(liTag);
+      }
+    }
 
-        let parentInputAppliance = ulAppliance.closest('div').id
-        let strAppareil = parentInputAppliance.replace('sub-search__', '');
+    for (let y = 0; tagArrayToSearch.ustensil.length !== y; y++) {
+      if (
+        cleanString(tagArrayToSearch.ustensil[y]).includes(
+          cleanString(liTag.innerText)
+        )
+      ) {
+        // console.log('Include dont build', 'tagArrayToSearch',tagArrayToSearch.length, tagArrayToSearch[y], y)
+      } else {
+        document.getElementById(id + "__taglist").append(liTag);
+      }
+    }
 
-        switch (id) {   
-            case 'Ingrédient':
-                liTag.classList.add('dropdown-item', 'bg-primary');
-                liTag.addEventListener('click', () => {
-                    // console.log('Litag', allIngredients, cleanTagName)
-                    cleanTagListDOM(strIngredient)
-                    liTag.setAttribute('aria-selected', 'true');
-                    console.log('Tags ', liTag.innerText)
-                    createTag(liTag, 'bg-primary')
-                    inputField.value = ''
-                })
+    if (!finalLength) document.getElementById(id + "__taglist").append(liTag);
 
-                break;
+    // }
+    // else
 
-            case 'Appareils':
-                liTag.classList.add('dropdown-item', 'bg-green');
-                liTag.addEventListener('click', () => {
-                    cleanTagListDOM(strAppareil)
-                    // console.log('UNIQUE APPLIANCE', uniqueAppliancesClone)
-                    liTag.setAttribute('aria-selected', 'true');
-                    // console.log('Tags ', tags)
-                    createTag(liTag, 'bg-green')
-                    inputField.value = ''
-
-                })
-
-
-                break;
-
-            case 'Ustensiles':
-                liTag.classList.add('dropdown-item', 'bg-red');
-                liTag.addEventListener('click', () => {
-                    cleanTagListDOM(strUstensil)
-                    liTag.setAttribute('aria-selected', 'true');
-                    createTag(liTag, 'bg-red')
-                    inputField.value = ''
-                })
-
-                break;
-
-        }
-
-        // remove tag from dom if exist in list 
-        // if(tagArrayToSearch.length !== 0 ) {
-
-            let finalLength =
-            tagArrayToSearch.ingredient.length +
-            tagArrayToSearch.appliance.length +
-            tagArrayToSearch.ustensil.length;
-
-            
-            for(let y = 0; tagArrayToSearch.ingredient.length !== y; y++) {
-                
-                if (cleanString((tagArrayToSearch.ingredient[y])).includes(cleanString(liTag.innerText))) {
-                    console.log('Include dont build', 'tagArrayToSearch',tagArrayToSearch.ingredient[y], liTag.innerText)
-                }else {
-                    document.getElementById(id + '__taglist').append(liTag);
-                }
-            }
-
-                        
-            for(let y = 0; tagArrayToSearch.appliance.length !== y; y++) {
-                
-                if (cleanString((tagArrayToSearch.appliance[y])).includes(cleanString(liTag.innerText))) {
-                    // console.log('Include dont build', 'tagArrayToSearch',tagArrayToSearch.length, tagArrayToSearch[y], y)
-                }else {
-                    document.getElementById(id + '__taglist').append(liTag);
-                }
-            }
-
-
-                        
-            for(let y = 0; tagArrayToSearch.ustensil.length !== y; y++) {
-                
-                if (cleanString((tagArrayToSearch.ustensil[y])).includes(cleanString(liTag.innerText))) {
-                    // console.log('Include dont build', 'tagArrayToSearch',tagArrayToSearch.length, tagArrayToSearch[y], y)
-                }else {
-                    document.getElementById(id + '__taglist').append(liTag);
-                }
-            }
-
-            if(!finalLength)
-            document.getElementById(id + '__taglist').append(liTag);
-
-            // }
-        // else 
-        
-        // if (tagArrayToSearch.length == 0) {
-        //     document.getElementById(id + '__taglist').append(liTag);
-        // }
-
-    };
-}
+    // if (tagArrayToSearch.length == 0) {
+    //     document.getElementById(id + '__taglist').append(liTag);
+    // }
+  }
+};
