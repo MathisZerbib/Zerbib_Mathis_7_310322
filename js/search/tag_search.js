92647078267049c85112dbf6e3cea10708b7c6a0
Mathis Zerbib <mathis.zerbib@gmail.com>
2022-10-20 20:35:04 +0200

    update tag search

--- js/search/tag_search.js
+++ js/search/tag_search.js
@@ -1,68 +1,38 @@
 const tagArraySearch = (searchedTags, data) => {
   let tempArray = data;
 
-  // let finalLength =
-  //   searchedTags.ingredient.length +
-  //   searchedTags.appliance.length +
-  //   searchedTags.ustensil.length;
-
   // Define var to check if current item is Included in a recipe
   let isIngIncluded;
   let isAppIncluded;
   let isUstensIncluded;
 
-  for (i = 0; searchedTags.ingredient.length !== i; i++) {
-    isIngIncluded = tempArray.filter((recipe) =>
-      recipe.ingredients.find((ingredient) =>
-        cleanString(ingredient.ingredient).includes(
-          cleanString(searchedTags.ingredient[i])
-        )
+  isIngIncluded = tempArray.filter((recipe) =>
+    searchedTags.ingredient.every((tagIng) =>
+      recipe.ingredients.some(
+        (recipeIng) => cleanString(tagIng) === cleanString(recipeIng.ingredient)
       )
-    );
-  }
+    )
+  );
 
-  for (i = 0; searchedTags.appliance.length !== i; i++) {
-    isAppIncluded = tempArray.filter((recipe) =>
-      cleanString(recipe.appliance).includes(
-        cleanString(searchedTags.appliance[i])
+  isAppIncluded = tempArray.filter((recipe) =>
+    searchedTags.appliance.every(
+      (tagApp) => cleanString(recipe.appliance) === cleanString(tagApp)
+    )
+  );
+
+  isUstensIncluded = tempArray.filter((recipe) =>
+    searchedTags.ustensil.every((tagUst) =>
+      recipe.ustensils.some(
+        (ustensil) => cleanString(ustensil) === cleanString(tagUst)
       )
-    );
-  }
+    )
+  );
 
-  for (i = 0; searchedTags.ustensil.length !== i; i++) {
-    isUstensIncluded = tempArray.filter((recipe) =>
-      recipe.ustensils.find((ustensil) =>
-        cleanString(ustensil).includes(cleanString(searchedTags.ustensil[i]))
-      )
-    );
-  }
+  filteredRecipes = isIngIncluded
+    .filter((value) => isAppIncluded.includes(value))
+    .filter((value) => isUstensIncluded.includes(value));
 
-  if (isIngIncluded) {
-    filteredRecipes = isIngIncluded.concat(isAppIncluded, isUstensIncluded);
-  }else if(isAppIncluded) {
-    filteredRecipes = isAppIncluded.concat(isIngIncluded, isUstensIncluded);
-
-  }else if( isUstensIncluded) {
-    filteredRecipes = isUstensIncluded.concat(isIngIncluded, isAppIncluded);
-
-  }
-
-  // Remove empty in array  
-  filteredRecipes = filteredRecipes.filter(n => n)
-
-
-const duplicates = filteredRecipes.filter((item, index) => index !== filteredRecipes.indexOf(item));
-
-// sort by "id" in ascending, and then by their "default" where  `false` first, then `true` second
-
-
-  if (duplicates.length !== 0 ) {
-    displayData(duplicates)
-    return duplicates;
-
-  }else {
-    displayData(filteredRecipes)
-  }
+  displayData(filteredRecipes);
   if (filteredRecipes.length == 0) {
     showError();
   }
