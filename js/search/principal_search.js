1d60eef1513db7ad3d69608d7f0885501cdd36e3
Mathis Zerbib <mathis.zerbib@gmail.com>
2022-10-20 21:59:24 +0200

    principal search method reviewed

--- js/search/principal_search.js
+++ js/search/principal_search.js
@@ -6,30 +6,32 @@ function filterElements(search, data) {
     return data;
   }
   if (search.length > 2) {
-let results = [];
-    resultRecipe.forEach((recipe) => {
-      if (cleanString(recipe.name).includes(cleanString(search))) {
+    let results = [];
+
+    for (let i = 0; resultRecipe.length > i; i++) {
+      if (
+        cleanString(resultRecipe[i].name).includes(cleanString(search)) ||
+        cleanString(resultRecipe[i].description).includes(cleanString(search))
+      ) {
         results.push(recipe);
-      } 
-    });
-    resultRecipe.forEach((recipe) => {
-      recipe.ingredients.forEach((ing) => {
-        if (
-          cleanString(ing.ingredient).includes(cleanString(search)) ||
-          cleanString(recipe.description).includes(cleanString(search))
-        ) {
-          results.push(recipe);
+      } else {
+        for (let y = 0; resultRecipe[i].ingredients.length > y; y++) {
+          if (
+            cleanString(resultRecipe[i].ingredients[y].ingredient).includes(
+              cleanString(search)
+            )
+          ) {
+            results.push(resultRecipe[i]);
+            break;
+          }
         }
-      });
-    });
-    filteredRecipes = results
+      }
+
+      filteredRecipes = results;
+    }
+
+    displayData(filteredRecipes);
+    // hideError();
+    return filteredRecipes;
   }
-
-
-
-  displayData(filteredRecipes);
-  // hideError();
-  return filteredRecipes;
 }
-
-// }
